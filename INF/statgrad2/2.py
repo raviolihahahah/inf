d801939f6dc95d0223e8f2738ed8f7260fa1b7b2
raviolihahahah <76631359+raviolihahahah@users.noreply.github.com>
2022-03-03 11:24:30 +0300

    Delete INF directory

--- INF/statgrad2/2.py
+++
@@ -1,7 +0,0 @@
-for x in 0, 1:
-    for y in 0, 1:
-        for w in 0, 1:
-            for z in 0, 1:
-                if( ((not(y)) <= (z == w)) and ((z <= x) == w)):
-                    print(x, y, w, z)
-    
